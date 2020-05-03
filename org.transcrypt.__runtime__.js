977529288d29270ad17e49a2a12067d761f3a69b
Thorvald Dox <thorvalddx94@gmail.com>
2020-05-03 15:51:38 +0200

    e

--- org.transcrypt.__runtime__.js
+++ org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2020-05-03 15:27:21
+// Transcrypt'ed from Python, 2020-05-03 15:34:41
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
