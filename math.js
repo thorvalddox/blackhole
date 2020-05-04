bcb90566dbd3b7f37bd2bb321bb05cc43f31fd7c
Thorvald Dox <thorvalddx94@gmail.com>
2020-05-04 10:42:10 +0200

    t

--- math.js
+++ math.js
@@ -140,3 +140,4 @@ export var modf = function (n) {
 //# sourceMappingURL=math.map
 //# sourceMappingURL=math.map
 //# sourceMappingURL=math.map
+//# sourceMappingURL=math.map
--- org.transcrypt.__runtime__.js
+++ org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2020-05-04 10:40:16
+// Transcrypt'ed from Python, 2020-05-04 10:42:09
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
