6f80527c01500bd29022e8442e87ab04edae2f44
Thorvald Dox <thorvalddx94@gmail.com>
2020-05-04 10:02:24 +0200

    k

--- math.js
+++ math.js
@@ -126,3 +126,4 @@ export var modf = function (n) {
 //# sourceMappingURL=math.map
 //# sourceMappingURL=math.map
 //# sourceMappingURL=math.map
+//# sourceMappingURL=math.map
--- random.js
+++ random.js
@@ -135,3 +135,4 @@ seed ();
 //# sourceMappingURL=random.map
 //# sourceMappingURL=random.map
 //# sourceMappingURL=random.map
+//# sourceMappingURL=random.map
